Test parameters and bug fixing for the h3 table (precision)

pyles/simulation.py
@@ -3,6 +3,7 @@ import logging
 import numpy as np
 import pywigxjpf as wig
 from scipy.special import spherical_jn, spherical_yn
+from scipy.special import jv, yv
 
 from pyles.parameters import Parameters
 from pyles.numerics import Numerics
@@ -34,12 +35,12 @@ class Simulation:
     self.h3_table = np.zeros(
       (2 * self.numerics.lmax + 1, self.lookup_particle_distances.shape[0], self.parameters.medium_mefractive_index.shape[0]), 
       dtype=complex)
+    self.size_param = np.outer(self.lookup_particle_distances, self.parameters.k_medium)
 
     for p in range(2 * self.numerics.lmax + 1):
-      temp = \
-        spherical_jn(p, np.outer(self.lookup_particle_distances, self.parameters.k_medium)) + 1j * \
-        spherical_yn(p, np.outer(self.lookup_particle_distances, self.parameters.k_medium))
-      self.h3_table[p, :, :] = temp
+      self.h3_table[p, :, :] = \
+        spherical_jn(p, self.size_param) + 1j * \
+        spherical_yn(p, self.size_param)
 
   def __setup(self):
     self.__compute_lookup_particle_distances()

tests/test_simulation.py
@@ -15,54 +15,64 @@ from pyles.numerics import Numerics
 from pyles.simulation import Simulation
 
 class TestSimulation(unittest.TestCase):
-  pass
 
-  # def test_setup(self):
-  #   module = 'simulation'
-  #   p = re.compile(module + r'_(.+)(\.big)?\.json')
-  #   for data_file in glob.glob('tests/data/simulation/%s_*.json' % module):
+  def test_setup(self):
+    module = 'test'
+    p = re.compile(module + r'_(.+)(\.big)?\.json')
+    for data_file in glob.glob('tests/data/%s_*.json' % module):
 
-  #     res = p.search(data_file)
-  #     identifier = res.group(1)
+      res = p.search(data_file)
+      identifier = res.group(1)
+      print(identifier)
 
-  #     data = pd.read_json(data_file, dtype=True)
-  #     lmax = data['input']['lmax']
-  #     spheres = np.array(data['input']['particles'])
-  #     wavelength = np.array(data['input']['wavelength'])
-  #     medium_ref_idx = np.array([complex(x.replace('i', 'j')) for x in data['input']['medium_ref_idx']])
+      data = pd.read_json(data_file, dtype=True)
+      lmax = data['input']['lmax']
+      spheres = np.array(data['input']['particles'])
+      wavelength = np.array(data['input']['wavelength'])
+      medium_ref_idx = np.array([complex(x.replace('i', 'j')) for x in data['input']['medium_ref_idx']])
 
-  #     polar_angle = data['input']['polar_angle']
-  #     azimuthal_angle = data['input']['azimuthal_angle']
-  #     polar_angles = np.array(data['input']['polar_angles'])
-  #     azimuthal_angles = np.array(data['input']['azimuthal_angles'])
+      polar_angle = data['input']['polar_angle']
+      azimuthal_angle = data['input']['azimuthal_angle']
+      polar_angles = np.array(data['input']['polar_angles'])
+      azimuthal_angles = np.array(data['input']['azimuthal_angles'])
 
-  #     particles = Particles(spheres[:,0:3], spheres[:,3], spheres[:,4:])
-  #     initial_field = InitialField(beam_width=0,
-  #                            focal_point=np.array((0,0,0)),
-  #                            polar_angle=polar_angle,
-  #                            azimuthal_angle=azimuthal_angle,
-  #                            polarization='TE')
+      particles = Particles(spheres[:,0:3], spheres[:,3], spheres[:,4:])
+      initial_field = InitialField(beam_width=0,
+                             focal_point=np.array((0,0,0)),
+                             polar_angle=polar_angle,
+                             azimuthal_angle=azimuthal_angle,
+                             polarization='TE')
       
-  #     parameters = Parameters(wavelength=wavelength,
-  #                   medium_mefractive_index=medium_ref_idx,
-  #                   particles=particles,
-  #                   initial_field=initial_field)
+      parameters = Parameters(wavelength=wavelength,
+                    medium_mefractive_index=medium_ref_idx,
+                    particles=particles,
+                    initial_field=initial_field)
 
-  #     numerics = Numerics(lmax=lmax,
-  #                   polar_angles=polar_angles,
-  #                   azimuthal_angles=azimuthal_angles,
-  #                   particle_distance_resolution=1,
-  #                   gpu=False)
+      numerics = Numerics(lmax=lmax,
+                    polar_angles=polar_angles,
+                    azimuthal_angles=azimuthal_angles,
+                    particle_distance_resolution=1,
+                    gpu=False)
 
-  #     simulation = Simulation(parameters, numerics)
+      simulation = Simulation(parameters, numerics)
 
-  #     h3_table = np.array(data['output']['setup']['h3_table']).astype(float)
+      h3_table = np.array(data['output']['simulation']['setup']['h3_table'], dtype=float)
+      h3_table_real = h3_table[:,:,0,:]
 
-  #     print(np.sum(np.isnan(np.real(simulation.h3_table))))
-  #     print(np.sum(np.isnan(np.real(h3_table[:,:,0,:]))))
+      simulation_h3_table_real = np.real(simulation.h3_table)
+      simulation_h3_table_imag = np.imag(simulation.h3_table)
 
-  #     np.testing.assert_array_almost_equal(simulation.lookup_particle_distances, data['output']['setup']['lookup_particle_distances'], 5, 'The lookup particles distances do not match.')
-  #     np.testing.assert_array_almost_equal(np.real(simulation.h3_table[:,:,0]), h3_table[:,:,0,0], 2, 'The h3 table does not match.')
+
+      for k in range(h3_table.shape[0]):
+        for l in range(h3_table.shape[1]):
+          if not np.allclose(h3_table[k,l,0,:], simulation_h3_table_real[k,l,:], rtol=1e-3, atol=1e-5, equal_nan=True):
+            for m in range(h3_table.shape[3]):
+              if not np.allclose(h3_table[k,l,0,m], simulation_h3_table_real[k,l,m], rtol=1e-3, atol=1e-5, equal_nan=True):
+                print(k, simulation.size_param[l, m], simulation_h3_table_real[k,l,m], h3_table[k,l,0,m])
+
+
+      np.testing.assert_array_almost_equal(simulation.lookup_particle_distances, data['output']['simulation']['setup']['lookup_particle_distances'], 8, 'The lookup particles distances do not match.')
+      # np.testing.assert_array_almost_equal(simulation_h3_table_real, h3_table_real, 0, 'The h3 table does not match.')
 
   # def test_initial_field_coefficients_planewave(self):
 