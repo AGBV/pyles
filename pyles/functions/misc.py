3e7bd88daa3390c05076e12101eeea2b5e4075df
Mirza <mirza.arnaut45@gmail.com>
2022-05-09 01:11:33 +0200

    Add a single index to multiple function as a compliment to multi2single_index

--- pyles/functions/misc.py
+++ pyles/functions/misc.py
@@ -6,6 +6,15 @@ def jmult_max(num_part, lmax):
 def multi2single_index(j_s,tau,l,m,lmax):
   return j_s * 2 * lmax * (lmax+2) + (tau-1) * lmax * (lmax+2) + (l-1)*(l+1) + m + l
 
+def single_index2multi(idx,lmax):
+  j_s = idx // (2 * lmax * (lmax + 2))
+  idx_new = idx % (2 * lmax * (lmax + 2))
+  tau = idx_new // (lmax * (lmax + 2)) + 1
+  idx_new = idx_new % (lmax * (lmax + 2))
+  l = np.floor(np.sqrt(idx_new+1))
+  m = idx_new - (l*l + l - 1)
+  return j_s, tau, l, m
+
 def transformation_coefficients(pilm, taulm, tau, l, m, pol, dagger: bool=False):
     if dagger:
       ifac = -1j
