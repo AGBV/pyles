6fc1807056f824ec3270d301e328c7422443b7d2
mar <mirza.arnaut@tu-dortmund.de>
2022-06-01 21:04:37 +0200

    Clean up the coupling matrix file and convert the indices from column- to row major (fixes order bug)

--- src/functions/coupling_matrix/cpu/parallel.py
+++ src/functions/coupling_matrix/cpu/parallel.py
@@ -4,8 +4,7 @@ from numba import jit, prange, complex128
 @jit(nopython=True, parallel=True, nogil=True, fastmath=True)
 def particle_interaction(lmax: int, particle_number: int, idx: np.ndarray, x: np.ndarray, translation_table: np.ndarray, plm: np.ndarray, sph_h: np.ndarray, e_j_dm_phi):
   wavelengths = sph_h.shape[-1]
-  nmax = 2 * lmax * (lmax + 2)
-  jmax = particle_number * nmax
+  jmax = particle_number * 2 * lmax * (lmax + 2)
 
   wx = np.zeros(x.size * wavelengths, dtype=complex128).reshape(x.shape + (wavelengths,))
 
@@ -28,10 +27,9 @@ def particle_interaction(lmax: int, particle_number: int, idx: np.ndarray, x: np
     for p in range(np.maximum(delta_m, delta_l + delta_tau), l1 + l2 + 1):
       val += translation_table[n2, n1, p] * \
           plm[p * (p + 1) // 2 + delta_m, s1, s2] * \
-          sph_h[p, s1, s2, w] * \
-          e_j_dm_phi[m2 - m1 + 2 * lmax, s1, s2] * \
-          x[j2]
-    
+          sph_h[p, s1, s2, w]
+    val *= e_j_dm_phi[m2 - m1 + 2 * lmax, s1, s2] * x[j2]    
+
     wx[j1, w] += val
 
   return wx
@@ -69,13 +67,15 @@ def particle_interaction(lmax: int, particle_number: int, idx: np.ndarray, x: np
 
 @jit(nopython=True, parallel=True, fastmath=True)
 def compute_idx_lookups(lmax: int, particle_number: int, idx: np.ndarray):
+  nmax = 2 * lmax * (lmax + 2)
 
   for s in prange(particle_number):
     for tau in range(1,3):
       for l in range(1, lmax+1):
         for m in range(-l, l+1):
           n = (tau - 1) * lmax * (lmax + 2) + (l - 1) * (l + 1) + l + m
-          i = s + n * particle_number
+          #i = s + n * particle_number
+          i = n + s * nmax
           idx[i, 0] = s
           idx[i, 1] = n
           idx[i, 2] = tau
